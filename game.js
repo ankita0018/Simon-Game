e9b7c69aeeee397816aa63480d838833b56a6b14
Ankita Singh <82949949+ankita0018@users.noreply.github.com>
2021-09-09 21:13:56 +0530

    Instructions

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+alert("Simon will duplicate the first signal and add one. Repeat these two signals by pressing the same color lenses, in order. Simon will duplicate these first two signals and add one. Continue playing as long as you can repeat each sequence of signals correctly. If you fail to repeat a sequence exactly, Simon responds with a  sound. This means you've lost, and the sequence of signals ends.");
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
